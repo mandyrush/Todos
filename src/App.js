c600f330a8c2a89008abf1f0be2f7e71f4a320f9
mandyrush <mandyrush85@gmail.com>
2021-10-26 19:56:41 -0600

    Create function to handle input change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isClicked: false,
+      todos: [],
+      text: ''
+    }
+  }
+
+  componentDidUpdate() {
+    // console.log('After Clicked', this.state.isClicked);
+    console.log('Updated text', this.state.text);
+  }
+
+  handleClick = () => {
+    console.log('Before Clicked', this.state.isClicked);
+    this.setState({
+      isClicked : !this.state.isClicked
+    });
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      text : event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.handleChange} />
+        <button onClick={this.handleClick}>Click Me</button>
+      </div>
+    );
+  }
 }
 
 export default App;
